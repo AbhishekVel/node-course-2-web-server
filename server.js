de30d7986e3a41da2049998c142658b1c9da9c56
abhishekvel <avelay@terpmail.umd.edu>
2017-08-10 09:56:55 -0400

    Added projects page

--- server.js
+++ server.js
@@ -53,6 +53,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Project Page',
+  });
+});
+
 // bad n - send back json with errorMessage property
 app.get('/bad', (req, res) => {
   res.send( {
